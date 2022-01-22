41611c8248068d582772e5ca70ba8ec209a4a76b
Ayushi Agarwal <87222543+ayushi15092002@users.noreply.github.com>
2022-01-22 14:04:32 +0530

    tab Bar and custom text widget added

---
+++ OP_Inventory_Billing_App/lib/screens/product_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:op_inventory_billing_app/widgets/TextWidget.dart';
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen ({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ProductScreenState();
+}
+
+class ProductScreenState extends State<ProductScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
---
+++ OP_Inventory_Billing_App/lib/tab_bar_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:op_inventory_billing_app/widgets/TextWidget.dart';
+
+class TabBarScreen extends StatefulWidget {
+  const TabBarScreen ({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => TabBarState();
+}
+
+class TabBarState extends State<TabBarScreen > with SingleTickerProviderStateMixin{
+  TabController? _controller;
+  int _selectedIndex = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+
+    _controller?.addListener(() {
+      setState(() {
+        _selectedIndex = _controller!.index;
+      });
+      print("Selected Index: " + _controller!.index.toString());
+    });
+  }
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  backgroundColor: Colors.white,
+                  title: const Center(
+                    child: MyText(text: "Inventory & Billing App", fontWeight: FontWeight.bold, size: 15.0,),
+                  ),
+                  pinned: true,
+                  floating: true,
+                  bottom: TabBar(
+                    indicatorColor: Colors.blueGrey,
+                    controller: _controller!,
+                    // isScrollable: true,
+                    tabs: [
+                      Tab(child: MyText(text: "Products")),
+                      Tab(child: MyText(text: "Billing")),
+                      Tab(child: MyText(text: "Sales")),
+                    ],
+
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(
+              controller: _controller!,
+              children: <Widget>[
+                Icon(Icons.flight, size: 350),
+                Icon(Icons.directions_transit, size: 350),
+                Icon(Icons.directions_car, size: 350),
+
+              ],
+            ),
+          )),
+    );;
+  }
+
+}
